13256f70717862bb68870d8da4bf1a60b65d457e
elgatoh <160553512+elgatwoo@users.noreply.github.com>
2024-02-23 13:09:30 +0000

    Better icon, visablity

--- newFile.js
+++ newFile.js
@@ -9,28 +9,32 @@ const task = document.getElementById("taskBar");
 const text = task.querySelectorAll("a");
 const img = task.querySelector("img")
 
-const converted = {
+/*------------------------- TaskBar effects ---------------------*/
+for (let i = 0; i < text.length; i++) {
 
-}
-/* code */
-for(let i = 0; i < text.length; i++) {
+  // hover effect
   text[i].addEventListener('mouseover', function() {
-    if(hover) {
+    if (hover) {
       text[i].style.backgroundColor = "rgba( 10, 10, 10, 1 )"
     }
   })
   text[i].addEventListener('mouseleave', function() {
     text[i].style.backgroundColor = "rgba( 0, 0, 0, 0.35 )"
-  })
-  text[i].addEventListener('transitionend', function(){
-    if(!expanded) {
-      task.style.width = "3%"
-      content[0].style.marginLeft = "3%"
+  }) // hover effect end
+
+
+  text[i].addEventListener('transitionend', function() {
+    // when button pressed and the menu is out
+    if (!expanded) {
+      task.style.width = "5%"
+      content[0].style.marginLeft = "5%"
+      text[i].style.visibility = "hidden"
     }
   })
-
-  task.addEventListener('transitionend', function(){
-    if(expanded) {
+  // i wonder what this does
+  task.addEventListener('transitionend', function() {
+    if (expanded) {
+      text[i].style.visibility = "visible"
       text[i].style.color = "rgba(255, 255, 255, 1)"
       hover = true
     }
@@ -38,13 +42,15 @@ for(let i = 0; i < text.length; i++) {
 }
 
 img.addEventListener('mousedown', function() {
-  if(!expanded) {
+  // when button pressed and the menu is out
+  if (!expanded) {
     task.style.width = "10%"
     content[0].style.marginLeft = "10%"
     expanded = true
-  } 
+  }
+  // when button pressed and the menu is out but not
   else {
-    for(let i = 0; i < text.length; i++) {
+    for (let i = 0; i < text.length; i++) {
       text[i].style.color = "rgba(255, 255, 255, 0)"
     }
 
